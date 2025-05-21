64e81f486e58f31069d1276c20395c76eb4aceca
LJM <1743390732@qq.com>
2025-05-21 17:45:24 +0800

    frontend done

---
+++ client/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const historyApiFallback = require('connect-history-api-fallback');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../public'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const path = require('path');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
@@ -7,9 +9,20 @@ connectDB();
 
 const app = express();
 
-//Body parser middlware
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// cors middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5000', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('hello');
